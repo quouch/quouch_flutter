6a95ad82d8394bbf2319fffac99b8c43dec339ac
papotte <berm.ross@gmail.com>
2024-10-11 11:18:29 +0200

    add cloudinary

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'dart:async';
+
+import 'package:cloudinary_flutter/cloudinary_context.dart';
+import 'package:cloudinary_flutter/cloudinary_object.dart';
+import 'package:cloudinary_url_gen/cloudinary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'app.dart';
 
+late CloudinaryObject cloudinary;
+
 FutureOr<void> main() async {
+  initCloudinary();
   runApp(const App());
+}
+
+Future<void> initCloudinary() async {
+  await dotenv.load(fileName: '.env');
+  CloudinaryContext.cloudinary =
+      Cloudinary.fromStringUrl(dotenv.env['CLOUDINARY_URL']!);
 }
